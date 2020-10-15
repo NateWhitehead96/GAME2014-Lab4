6c2084fddf125df686e2ec3af0f7f7bfc90ac613
NateWhitehead96 <n_whitehead96@hotmail.ca>
2020-10-15 11:07:19 -0400

    Completed Lab 4
    Followed along in class to show factory pattern method for spawning bullets

---
+++ Assets/_Scripts/BulletFactory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BulletFactory : MonoBehaviour
+{
+    [Header("Bullet Types")]
+    public GameObject regularBullet;
+    public GameObject fatBullet;
+    public GameObject pulsingBullet;
+
+    //public BulletManager bulletManager;
+
+    public GameObject createBullet(BulletType type = BulletType.RANDOM)
+    {
+        if(type == BulletType.RANDOM)
+        {
+            var randomBullet = Random.Range(0, 3);
+            type = (BulletType)randomBullet;
+        }
+
+        GameObject tempBullet = null;
+        switch (type)
+        {
+            case BulletType.REGULAR:
+                tempBullet = Instantiate(regularBullet);
+                break;
+            case BulletType.FAT:
+                tempBullet = Instantiate(fatBullet);
+                break;
+            case BulletType.PULSING:
+                tempBullet = Instantiate(pulsingBullet);
+                break;
+            
+        }
+
+        tempBullet.transform.parent = transform;
+        tempBullet.SetActive(false);
+        return tempBullet;
+    }
+}
